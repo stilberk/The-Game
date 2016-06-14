ee73f4d6f850a4342de46493b90e2d66fe0e63fd
Neli Noykova <noykova@users.noreply.github.com>
2016-06-14 18:50:32 +0300

    Restore old version where README.md worked properly

---
+++ SpriteAnimation/Feynman.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SpriteAnimation
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    ///     The main class.
+    /// </summary>
+    public static class Feynman
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
